569340bd6d8d20c166b88af53e6f8db38b9a8780
Jonathan Kvist <jonathan.kvist@gmail.com>
2017-10-06 11:35:41 +0200

    Frontend with new html, css and jquery

---
+++ Frontend/myJQuery.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    
+    setTimeout(function(){
+        $('.main-nav').fadeIn(1000).removeClass('hidden'); 
+    }, 500);
+    
+     setTimeout(function(){
+        $('.inputfield').fadeIn(1000).removeClass('hidden');
+    }, 1000);
+    
+});
+
