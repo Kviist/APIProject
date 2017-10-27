ccd7fdc3ac2683c0089977e68c1908e06bb61211
Jonathan Kvist <jonathan.kvist@gmail.com>
2017-10-27 12:03:04 +0200

    Fixed a few weathers

--- VaderMusikMaskinen/src/main/java/dataclasses/PlaylistID.java
+++ VaderMusikMaskinen/src/main/java/dataclasses/PlaylistID.java
@@ -12,16 +12,17 @@ public class PlaylistID {
         playlistIds = new HashMap<>();
 
         playlistIds.put("KLART","37i9dQZF1DXdPec7aLTmlC");
-        playlistIds.put("HALVMULET","37i9dQZF1DWZCYmSEd1QSS");
+        playlistIds.put("HALVMULET","37i9dQZF1DWTkxQvqMy4WW");
         playlistIds.put("MOLNIGT","37i9dQZF1DWUNIrSzKgQbP");
         playlistIds.put("DIMMA","37i9dQZF1DWWuOw4E4LGde");
         playlistIds.put("ÅSKA","37i9dQZF1DWWJOmJ7nRx0C");
-        playlistIds.put("REGN","37i9dQZF1DX9NmDLwNQnXE");
+        playlistIds.put("REGN","37i9dQZF1DX3reMgdkcDa4");
         playlistIds.put("KRAFTIGTREGN","37i9dQZF1DX4aYNO8X5RpR");
         playlistIds.put("SNÖBLANDATREGN","37i9dQZF1DX2mFmJUZg4Mp");
         playlistIds.put("SLASK","37i9dQZF1DXbcPC6Vvqudd");
         playlistIds.put("GANSKAJULIGT","37i9dQZF1DXbPHTEEyQ6Hv");
         playlistIds.put("ULTRAJULIGT","37i9dQZF1DXbPHTEEyQ6Hv");
+
     }
 
     public  String getId(String weather){
